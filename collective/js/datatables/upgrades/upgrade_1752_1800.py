3be48bc646a712351e01e4fc6ef92bdc2050b377
Nejc Zupan <nejc.zupan@gmail.com>
2011-06-15 16:05:15 +0200

    don't re-import the whole step, just re-generate javascripts

--- collective/js/datatables/upgrades/upgrade_1752_1800.py
+++ collective/js/datatables/upgrades/upgrade_1752_1800.py
@@ -1,4 +1,9 @@
 from Products.CMFCore.utils import getToolByName
 
+
 def upgrade(context):
-    context.runImportStepFromProfile('profile-collective.js.datatables:default', 'jsregistry')
+    """Since the resource name stays the same, we just need to re-generate our
+    Javascripts.
+    """
+    jsregistry = getToolByName(context, 'portal_javascripts')
+    jsregistry.cookResources()
