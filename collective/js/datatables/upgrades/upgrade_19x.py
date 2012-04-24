b3e4a398ceb4b939173f2a25dc22d81eb4382447
JeanMichel FRANCOIS <toutpt@gmail.com>
2012-04-24 16:11:58 +0200

    fix handler name

--- collective/js/datatables/upgrades/upgrade_19x.py
+++ collective/js/datatables/upgrades/upgrade_19x.py
@@ -1,6 +1,6 @@
 from Products.CMFCore.utils import getToolByName
 
-def upgrade(context):
+def upgrade_1901(context):
     """Since the resource name stays the same, we just need to re-generate our
     Javascripts.
     """
