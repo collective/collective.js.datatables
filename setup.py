c1f637ad29134de8c3e57fa40d54315568f618f7
JeanMichel FRANCOIS <toutpt@gmail.com>
2012-10-19 10:47:22 +0200

    Back to development: 2.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import os
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '2.0'
+version = '2.1.dev0'
 
 setup(name='collective.js.datatables',
       version=version,
