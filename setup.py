5139146a28c6ae8e54ec40ab8f929dac494b7e77
Jens W. Klein <jk@kleinundpartner.at>
2015-06-25 16:10:53 +0200

    Back to development: 3.1.10.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import os
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '3.1.10.5'
+version = '3.1.10.6.dev0'
 
 setup(name='collective.js.datatables',
       version=version,
