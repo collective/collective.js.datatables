79ed2b477fd87fddc6cadbee111570311467efdb
Rémi Dubois <remi.dubois@gmail.com>
2025-04-11 13:37:28 +0200

    fix: requests is not needed

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,7 @@
 from collective.js.datatables.testing import ACCEPTANCE_TESTING
 from collective.js.datatables.testing import FUNCTIONAL_TESTING
 from collective.js.datatables.testing import INTEGRATION_TESTING
-from pathlib import Path
-from plone import api
 from pytest_plone import fixtures_factory
-from requests.exceptions import ConnectionError
-from zope.component.hooks import setSite
-
-import pytest
-import requests
-
 
 pytest_plugins = ["pytest_plone"]
 
