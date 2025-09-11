a46aec9ae18664c33ecde04450f3bbd271507905
Laurent Lasudry <lasudry@gmail.com>
2025-09-11 13:50:58 +0200

    Cleanup old upgrade steps

--- collective/js/datatables/upgrades.py
+++ collective/js/datatables/upgrades.py
@@ -4,16 +4,6 @@ PROFILE = "profile-collective.js.datatables:default"
 
 
 def common(context):
-    """Just apply the prefix"""
+    """Just apply the profile"""
     setup = getToolByName(context, 'portal_setup')
     setup.runAllImportStepsFromProfile(PROFILE)
-
-def to2001(context):
-    """Just apply the prefix"""
-    setup = getToolByName(context, 'portal_setup')
-    css = getToolByName(context, 'portal_css')
-    js = getToolByName(context, 'portal_javascripts')
-    setup.runImportStepFromProfile(PROFILE, 'jsregistry')
-    setup.runImportStepFromProfile(PROFILE, 'cssregistry')
-    css.cookResources()
-    js.cookResources()
